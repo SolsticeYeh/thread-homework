0f9d6e6dfc3dc86bd247e2279f714e215564a7c9
nealbity <yezhou2@outlook.com>
2015-12-02 12:31:39 +0800

    thread-homework

---
+++ thread/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(interesting).Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("@");
+            }
+        }
+
+        static void interesting()
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Thread.Sleep(2000);
+                Console.Write("$");
+            }
+        }
+    }
+}
